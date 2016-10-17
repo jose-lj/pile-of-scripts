0d1247a015cb666a697f29ba1728f5d4d5768d3f
Jose Lopez <joselj@ugr.es>
2016-10-17 16:35:27 +0200

    Shows counters as decimal, performance improvements

--- droppermem.sh
+++ droppermem.sh
@@ -14,18 +14,17 @@ divider=$divider$divider
 width=41
 
 STRING="$(devmem $ACCEPT_COUNT0)"
-printf "Port 0 accepted:\t%8s\n" ${STRING:2}
+printf "Port 0 accepted:\t%d\n" ${STRING}
 STRING="$(devmem $ACCEPT_COUNT1)"
-printf "Port 1 accepted:\t%8s\n" ${STRING:2}
+printf "Port 1 accepted:\t%d\n" ${STRING}
 STRING="$(devmem $DROP_COUNT0)"
-printf "Port 0 dropped:\t%8s\n" ${STRING:2}
+printf "Port 0 dropped:\t\t%d\n" ${STRING}
 STRING="$(devmem $DROP_COUNT1)"
-printf "Port 1 dropped:\t%8s\n" ${STRING:2}
+printf "Port 1 dropped:\t\t%d\n" ${STRING}
 
 HEADER="\n%7s     %12s   %9s\n"
 FORMAT="%02d     %1d     %2s:%2s:%2s:%2s:%2s:%2s      %4s\n"
 STRING="$(devmem $BASEADDR)"
-printf '\nUsed positions: %s\n' $STRING
 printf "$HEADER" "Pos. Used" "   MAC Addr.   " "  Seqnum"
 printf "%$width.${width}s\n" "$divider"
 
@@ -35,7 +34,7 @@ do
 	STRING1="$(devmem $BASEADDR2)"
 	BASEADDR3=$((BASEADDR3+8))
 	STRING2="$(devmem $BASEADDR3)"
-	RESULT=$((STRING & 2**$i))
+	RESULT=$((STRING & (1 << $i)))
 	if [ "$RESULT" -eq 0 ]; then
 		RESULT=0
 	else
@@ -44,5 +43,7 @@ do
 	printf "$FORMAT" $i $RESULT ${STRING1:2:2} ${STRING1:4:2} ${STRING1:6:2} ${STRING1:8:2} ${STRING2:2:2} ${STRING2:4:2} ${STRING2:6:4}
 	#printf '%s Seqnum %s\n' ${STRING:2:4} ${STRING:6:4}
 done
+printf "%$width.${width}s\n" "$divider\n"
+
 
 
